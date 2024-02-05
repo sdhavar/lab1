ee6694c06b4b49db5b899bf6e576cf7fab3712a1
Santiago David Heras Arciniegas <herass@uninorte.edu.co>
2024-02-05 18:53:32 -0500

    Cambio programador A

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const esPrimo = primo => {
 
 while (i < cantidad){
 
-    if(esPrimo(primoTemp)){
+    if(esPrimo(primoTemp) && true && i < cantidad){
         console.log(i+1, "->", primoTemp);
 
         i++
