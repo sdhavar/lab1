3e46d8dad6bdc8820795bcf4d74d1d7ed56fef0a
Santiago David Heras Arciniegas <herass@uninorte.edu.co>
2024-02-05 18:19:25 -0500

    Casi termino

--- script.js
+++ script.js
@@ -4,6 +4,26 @@ const cantidad = 5
 let i = 0
 let primoTemp = 1
 
-while (i < cantidad){
-    
+// while (i < cantidad){
+
+// }
+
+
+const esPrimo = primo => {
+
+    let cantDivisores = 0
+
+    for (let i = 1; i <= primo; i++) {
+        if (primo % i == 0){
+            cantDivisores++
+        }
+    }
+
+    if (cantDivisores == 2) {
+        return true
+    }
+
+    return false
 }
+
+console.log(esPrimo(2))
